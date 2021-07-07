aad59660317e93bd45bff7bcecb9079eda982279
ekansh9696 <ekanshkdmaworld@gmail.com>
2021-07-08 02:12:58 +0530

    refactored

--- src/main/java/co/interleap/courses/tdd/StatementGenerator.java
+++ src/main/java/co/interleap/courses/tdd/StatementGenerator.java
@@ -1,9 +1,11 @@
 package co.interleap.courses.tdd;
 
 public class StatementGenerator {
+    public static final int FARE_PER_KM=10;
+    public static final int FARE_PER_MINUTE=1;
     double getFare(double distance,int minutes)
     {
-        return distance*10+minutes*1;
+        return distance*FARE_PER_KM+minutes*FARE_PER_MINUTE;
     }
 
 }
