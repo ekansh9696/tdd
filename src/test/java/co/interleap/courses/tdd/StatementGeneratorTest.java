d3031a1f3eff71a2e06945a781908ec125a02f00
ekansh9696 <ekanshkdmaworld@gmail.com>
2021-07-08 02:48:43 +0530

    refactoring on test cases by inlining

--- src/test/java/co/interleap/courses/tdd/StatementGeneratorTest.java
+++ src/test/java/co/interleap/courses/tdd/StatementGeneratorTest.java
@@ -11,32 +11,28 @@ public class StatementGeneratorTest
     public void noTravel()
     {
         StatementGenerator v= new StatementGenerator();
-        Ride rides[]={new Ride(0,0)};
-        double fare= v.getFare(Arrays.asList(rides));
+        double fare= v.getFare(Arrays.asList(new Ride(0,0)));
         Assert.assertEquals(0.0,fare,DELTA);
     }
     @Test
     public void minTravel()
     {
         StatementGenerator v = new StatementGenerator();
-        Ride rides[]={new Ride(1,5)};
-        double fare= v.getFare(Arrays.asList(rides));
+        double fare= v.getFare(Arrays.asList(new Ride(1,5)));
         Assert.assertEquals(15.0,fare,DELTA);
     }
     @Test
     public void distanceInDecimalsSingleRide()
     {
         StatementGenerator v = new StatementGenerator();
-        Ride rides[]={new Ride(2.5,30)};
-        double fare= v.getFare(Arrays.asList(rides));
+        double fare= v.getFare(Arrays.asList(new Ride(2.5,30)));
         Assert.assertEquals(55.0,fare,DELTA);
     }
     @Test
     public void multipleRides()
     {
         StatementGenerator v = new StatementGenerator();
-        Ride rides[]={new Ride(2.5,30),new Ride(1,5),new Ride(12,45)};
-        double fare= v.getFare(Arrays.asList(rides));
+        double fare= v.getFare(Arrays.asList(new Ride(2.5,30),new Ride(1,5),new Ride(12,45)));
         Assert.assertEquals(235,fare,DELTA);
     }
 
