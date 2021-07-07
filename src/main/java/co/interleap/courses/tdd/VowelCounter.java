a009f3aa28f565e1d7bd47d23355a99ebb2c5841
ekansh9696 <ekanshkdmaworld@gmail.com>
2021-07-08 00:04:16 +0530

    performed tests and implementation for 3 tests

--- src/main/java/co/interleap/courses/tdd/VowelCounter.java
+++ src/main/java/co/interleap/courses/tdd/VowelCounter.java
@@ -1,4 +1,15 @@
 package co.interleap.courses.tdd;
 
 public class VowelCounter {
+    String input;
+    VowelCounter(String value)
+    {
+        input=value;
+    }
+    public int getCount()
+    {
+        int count=0;
+        count=input.length();
+        return count;
+    }
 }
--- src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
+++ src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
@@ -1,9 +1,25 @@
 package co.interleap.courses.tdd;
 
-import org.junit.Test;
+import org.junit.*;
 
 public class VowelCounterTest {
 
     @Test
-    public void dummy(){}
+    public void emptyString()
+    {
+        VowelCounter v = new VowelCounter("");
+        Assert.assertEquals(0,v.getCount());
+    }
+    @Test
+    public void singleVowelString()
+    {
+        VowelCounter v = new VowelCounter("e");
+        Assert.assertEquals(1,v.getCount());
+    }
+    @Test
+    public void allVowelString()
+    {
+        VowelCounter v = new VowelCounter("aeiou");
+        Assert.assertEquals(5,v.getCount());
+    }
 }
