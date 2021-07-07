b614a3864243faffdc0f540a0f0ee8d1052d162f
ekansh9696 <ekanshkdmaworld@gmail.com>
2021-07-08 00:56:20 +0530

    refactoring
    extracted checking statements and inlined the function

--- src/main/java/co/interleap/courses/tdd/VowelCounter.java
+++ src/main/java/co/interleap/courses/tdd/VowelCounter.java
@@ -11,15 +11,16 @@ public class VowelCounter {
     public int getCount()
     {
         int count=0;
-        //char v[]={'a','A','e','E','i','I','o','O','u','U'};
         for(int i=0;i<input.length();i++)
         {
-            char current=input.charAt(i);
-            //if(Arrays.asList(v).contains(current))
-            //    count++;
-            if(current=='a' || current=='A' || current=='e' || current=='E' || current=='i' || current=='I' || current=='o' || current=='O' || current=='u' || current=='U')
-                count++;
+            count = updateIfVowel(count, input.charAt(i));
         }
+        return count;
+    }
+
+    private int updateIfVowel(int count, char current) {
+        if(current =='a' || current =='A' || current =='e' || current =='E' || current =='i' || current =='I' || current =='o' || current =='O' || current =='u' || current =='U')
+            count++;
         return count;
     }
 }
